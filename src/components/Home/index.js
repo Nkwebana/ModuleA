9e1df8df976ac4fa7f776270244fa4221aeea7bd
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-11-16 14:45:11 +0200

    added login screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,12 +6,12 @@
 
 import React, { useState } from 'react';
 import {
-  TextInput,
-  StyleSheet,
   Button,
   View,
 } from 'react-native';
 import PropTypes from "prop-types";
+
+import { StyledTextInput } from './styledComponents'
 
 function Home({ handleLogin }) {
   const [username, setUsername] = useState(null);
@@ -19,15 +19,13 @@ function Home({ handleLogin }) {
 
   return (
     <View>
-      <TextInput
-        style={styles.input}
+      <StyledTextInput
         onChangeText={setUsername}
         value={username}
         placeholder="Enter username"
       />
 
-      <TextInput
-        style={styles.input}
+      <StyledTextInput
         onChangeText={setPassword}
         value={password}
         placeholder="Enter password"
@@ -42,14 +40,7 @@ Home.propTypes = {
   handleLogin: PropTypes.func,
 };
 
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+
 
 
 export default Home;
---
+++ src/components/Home/styledComponents/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledTextInput = styled.View`
+  height: 40px;
+  margin: 12px;
+  border-Width: 1px;
+  padding: 10px;
+`;
+
+export { StyledTextInput }
